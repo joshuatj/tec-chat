23daabf71117d5e59c554145fd176a9f02b32b36
Tan Chee Eng <me@tan-ce.com>
2014-05-04 23:34:41 +0800

    Default Preference dialog

--- src/com/tan_ce/tecingamechat/ChatActivity.java
+++ src/com/tan_ce/tecingamechat/ChatActivity.java
@@ -513,6 +513,8 @@ public class ChatActivity extends Activity {
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
+			Intent settingsIntent = new Intent(this, SettingsActivity.class);
+			startActivity(settingsIntent);
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
